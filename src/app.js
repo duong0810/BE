9221057dc1f58ce31d78021e3d2ae993c3d26d6e
Minh Duong <duong0810@example.com>
2025-06-16 22:52:35 +0700

    Update

--- src/app.js
+++ src/app.js
@@ -25,10 +25,12 @@ const app = express();
 app.use(cors({
   origin: [
     'https://h5.zdn.vn',
+    'http://localhost:2999',
     'http://localhost:3000', 
     'http://localhost:3001', 
     'http://localhost:5173',
-    'http://localhost:5000'
+    'http://localhost:5000',
+    'https://zalo.me'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
