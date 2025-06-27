78a0ec85fca76122f82ea73f5e32d9cff8890ae9
duong <minhduong5534@gmail.com>
2025-06-27 14:30:36 +0700

    .

--- src/routes/voucherRoutes.js
+++ src/routes/voucherRoutes.js
@@ -11,7 +11,6 @@ import {
 import { authMiddleware } from "../middlewares/auth.js";
 import multer from "multer";
 import { getBannerHeaders, updateBannerHeaders } from "../controllers/voucherController.js";
-import { spinWheelWithLimit } from "../controllers/voucherController.js";
 
 const upload = multer({ dest: "uploads/" });
 
@@ -19,9 +18,6 @@ const router = express.Router();
 
 router.get("/spin", spinVoucher);
 
-
-// Route quay vòng quay (spin) với giới hạn lượt quay
-router.post("/vouchers/spin-wheel-limit", spinWheelWithLimit);
 
 // Route lấy banner headers
 router.get("/banner-headers", getBannerHeaders);
