f543c7e3e570a956322fce02596a7692937d6263
Minh Duong <duong0810@example.com>
2025-07-07 21:03:18 +0700

    .

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ app.use(cookieParser());
 app.options('*', (req, res) => {
   res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-requested-with');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-requested-with, zalo-access-token, access-token');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.sendStatus(200);
 });
