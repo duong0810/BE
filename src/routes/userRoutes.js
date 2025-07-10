10f1c039be7e3a9d05a9ce91f40f979ea142f147
Minh Duong <duong0810@example.com>
2025-07-10 13:52:01 +0700

    .

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,11 @@
 import express from "express";
-import { verifyZaloToken } from "../middlewares/zaloAuth.js";
-import { getUserProfile, updateUserProfile } from "../controllers/userController.js";
+import { authMiddleware, adminMiddleware } from "../middlewares/auth.js";
+import { getUserProfile, updateUserProfile, getAllUsers } from "../controllers/userController.js";
 
 const router = express.Router();
 
-// API cập nhật thông tin tài khoản (user tự cập nhật)
 router.put("/me", verifyZaloToken, updateUserProfile);
-router.get("/me", verifyZaloToken, getUserProfile); 
+router.get("/me", verifyZaloToken, getUserProfile);
 
 // API lấy danh sách toàn bộ user (chỉ cho admin)
 router.get("/", authMiddleware, adminMiddleware, getAllUsers);
